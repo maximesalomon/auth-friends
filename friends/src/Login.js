4c95220526c2c99350f405f0fd698061e33bf76f
Maxime Salomon <maxime@croissant.io>
2020-01-17 13:42:26 +0100

    axios post request + error handling

--- friends/src/Login.js
+++ friends/src/Login.js
@@ -12,28 +12,40 @@ const LoginForm = styled.div`
 `;
 
 const FormButton = styled.button`
-    margin: 20px auto;
-    padding: 14px;
-    width: 40%;
-    background-color: white;
-    border: solid blue 1px;
-    border-radius: 4px;
-    :hover {
-        background-color: blue;
-        color: white;
-    }
-`
+  margin: 20px auto;
+  padding: 14px;
+  width: 40%;
+  background-color: white;
+  border: solid blue 1px;
+  border-radius: 4px;
+  :hover {
+    background-color: blue;
+    color: white;
+  }
+`;
+
+const Error = styled.p`
+  color: red;
+`;
 
 const Login = () => {
+  const [error, setError] = useState();
   return (
     <div className="App">
       <h1>Login</h1>
+      {error && <Error>{error}</Error>}
       <Formik
         initialValues={{ email: "", password: "" }}
         onSubmit={(data, { setSubmitting }) => {
           setSubmitting(true);
-          // make async call
-          console.log("Submit = ", data);
+          axios
+            .post("http://localhost:5000/api/login", data)
+            .then(res => {
+              console.log(res);
+            })
+            .catch(err => {
+              setError(err.response.data.error)
+            });
           setSubmitting(true);
         }}
       >
@@ -41,7 +53,11 @@ const Login = () => {
           <Form>
             <LoginForm>
               <Field name="email" type="email" placeholder="email"></Field>
-              <Field name="password" type="password" placeholder="password"></Field>
+              <Field
+                name="password"
+                type="password"
+                placeholder="password"
+              ></Field>
               <FormButton disabled={isSubmitting}>Submit</FormButton>
             </LoginForm>
           </Form>
