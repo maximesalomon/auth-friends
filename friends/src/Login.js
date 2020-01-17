2bdda0bf81aec54824346208525b4ac676b3f23f
Maxime Salomon <maxime@croissant.io>
2020-01-17 12:48:57 +0100

    formik Form + Field

--- friends/src/Login.js
+++ friends/src/Login.js
@@ -1,30 +1,33 @@
-import React, { useState } from 'react';
-import { Formik, Form, Field} from 'formik';
-import axios from 'axios';
+import React, { useState } from "react";
+import { Formik, Form, Field } from "formik";
+import axios from "axios";
 
 const Login = () => {
   return (
     <div className="App">
       <h1>Login</h1>
       <Formik
-        initialValues={{ username: '', password: ''}}
-        onSubmit={data => {
-          console.log('Submit = ', data);
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(data, { setSubmitting }) => {
+          setSubmitting(true);
+          // make async call
+          console.log("Submit = ", data);
+          setSubmitting(true);
         }}
       >
-        {({ values, handleChange, handleBlur, handleSubmit }) => (
-            <>
-                <Form onSubmit={handleSubmit}>
-                    <input name='username' type='text' placeholder='username' value={values.username} onChange={handleChange} onBlur={handleBlur}></input>
-                    <input name='password' type='password' placeholder='passsword' value={values.password} onChange={handleChange} onBlur={handleBlur}></input>
-                    <button>Submit</button>
-                </Form>
-                <pre>{JSON.stringify(values)}</pre>
-            </>
+        {({ values, isSubmitting }) => (
+          <>
+            <Form>
+              <Field name="username" type="input"></Field>
+              <Field name="password" type="password"></Field>
+              <button disabled={isSubmitting}>Submit</button>
+            </Form>
+            <pre>{JSON.stringify(values, null, 2)}</pre>
+          </>
         )}
       </Formik>
     </div>
   );
-}
+};
 
 export default Login;
